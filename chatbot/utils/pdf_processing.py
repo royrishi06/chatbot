fbf801ecaffb61dbec61c70da2162646ebbf84d0
royrishi06 <120101553+royrishi06@users.noreply.github.com>
2025-01-08 02:26:07 +0530

    Delete chatbot directory

--- chatbot/utils/pdf_processing.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfReader
-
-def extract_text_from_pdf(pdf_file):
-    """Extracts text from an uploaded PDF file."""
-    reader = PdfReader(pdf_file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
